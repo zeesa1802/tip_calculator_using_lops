46aef6ff9ad78821fde2032d16da3134b39a3298
Zeeshan <zeesa18.2@gmail.com>
2022-06-14 17:02:48 +0500

    Completed loops.

---
+++ script.js
@@ -0,0 +1,24 @@
+const calcTip = bill => bill >=30 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+const calculateAverage = function(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totalBills = [];
+
+for(let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totalBills[i] = bills[i] + tips[i];
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totalBills);
+
+console.log(calculateAverage(bills));
