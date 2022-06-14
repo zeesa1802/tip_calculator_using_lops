15c71ca96ec2454fe6b7c4f389c198e82a6b1f97
Zeeshan <zeesa18.2@gmail.com>
2022-06-14 17:18:34 +0500

    Added prettierrc settings

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const calcTip = bill => bill >=30 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const calcTip = bill => (bill >= 30 && bill <= 300 ? bill * 0.15 : bill * 0.2);
 
-const calculateAverage = function(arr) {
+const calculateAverage = function (arr) {
   let sum = 0;
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum / arr.length;
-}
+};
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totalBills = [];
 
-for(let i = 0; i < bills.length; i++) {
+for (let i = 0; i < bills.length; i++) {
   tips[i] = calcTip(bills[i]);
   totalBills[i] = bills[i] + tips[i];
 }
